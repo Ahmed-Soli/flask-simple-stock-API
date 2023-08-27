c9009529014bb9fa04f3621fade3780f633ea311
Ahmed Soliman <ahmedsoliman202@gmail.com>
2023-08-28 00:17:50 +0300

    intialize JWT once the app runs

--- api_service/app.py
+++ api_service/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from api_service import api
 from api_service.extensions import db
 from api_service.extensions import migrate
+from api_service.extensions import jwt
 
 
 def create_app(testing=False):
@@ -12,7 +13,7 @@ def create_app(testing=False):
 
     if testing is True:
         app.config["TESTING"] = True
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////api_service.sqlite3"
     configure_extensions(app)
     register_blueprints(app)
 
@@ -22,6 +23,7 @@ def create_app(testing=False):
 def configure_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
 
 def register_blueprints(app):
