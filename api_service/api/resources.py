5508a0f1906aca65f0783dd10b7483905e4abe2b
Ahmed Soliman <ahmedsoliman202@gmail.com>
2023-08-28 00:12:28 +0300

    add login resource

--- api_service/api/resources.py
+++ api_service/api/resources.py
@@ -1,8 +1,30 @@
 from flask import request
 from flask_restful import Resource
 from api_service.api.schemas import StockInfoSchema
-from api_service.extensions import db
+from api_service.extensions import db, pwd_context
+from flask_jwt_extended import create_access_token
+from api_service.models import User
+from flask import abort, jsonify, request
 
+
+class Login(Resource):    
+    def post(self):
+        json_data = request.get_json()
+        username = json_data["username"]
+        password = json_data["password"]
+
+        user = db.session.execute(db.select(User).filter_by(username=username)).scalar()
+        if not user:
+            abort(401, "Invalid user credentials")
+
+        valid_pwd = pwd_context.verify(password, user.password)
+        if not valid_pwd:
+            abort(401, "Invalid user credentials")
+
+        access_token = create_access_token(
+            username, additional_claims={"user_id": user.id, "role": user.role}
+        )
+        return jsonify(token=access_token)
 
 class StockQuery(Resource):
     """
