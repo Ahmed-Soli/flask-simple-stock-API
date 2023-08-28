620d86a246af2c8bd03ebb4a10f60cae59397f3e
Ahmed Soliman <ahmedsoliman202@gmail.com>
2023-08-28 15:32:30 +0300

    refactor stock request handler to work for tests

--- stock_service/clients/stooq.py
+++ stock_service/clients/stooq.py
@@ -1,3 +1,4 @@
+import csv
 from datetime import datetime
 
 import requests
@@ -5,14 +6,14 @@ import requests
 
 class StooqObject(object):
     def __init__(self, stooq_dict):
-        self.symbol = stooq_dict.get("symbol")
-        self.name = stooq_dict.get("name")
-        self.open = stooq_dict.get("open")
-        self.high = stooq_dict.get("high")
-        self.low = stooq_dict.get("low")
-        self.close = stooq_dict.get("close")
-        self.date = stooq_dict.get("date")
-        self.time = stooq_dict.get("time")
+        self.symbol = self._get_sanitized_data(stooq_dict, "Symbol")
+        self.name = self._get_sanitized_data(stooq_dict, "Name")
+        self.open = self._get_sanitized_data(stooq_dict, "Open")
+        self.high = self._get_sanitized_data(stooq_dict, "High")
+        self.low = self._get_sanitized_data(stooq_dict, "Low")
+        self.close = self._get_sanitized_data(stooq_dict, "Close")
+        self.date = self._get_sanitized_data(stooq_dict, "Date")
+        self.time = self._get_sanitized_data(stooq_dict, "Time")
 
         if self.date:
             try:
@@ -26,20 +27,38 @@ class StooqObject(object):
             except ValueError:
                 self.time = None
 
+    def _get_sanitized_data(self, d, key):
+        if key not in d:
+            return None
+
+        value = d.get(key, None)
+        if value == "N/D":
+            return None
+        return value
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
 
 class StooqClient(object):
+    resource_path = "q/l"
 
-    def __init__(self):
-        self.base_url = 'https://stooq.com/q/l'
+    def __init__(self, base_url):
+        self.base_url = base_url
 
     def get_stock(self, code):
-        params = {"s": code, "f": "sd2t2ohlcvn", "e": "json", "h": ""}
-        resp =  requests.get(url=self.base_url, params=params)
-        try:
-            resp.raise_for_status()
-            json_obj = resp.json()
-            #{"symbols":[{"symbol":"AAPL.US","date":"2023-08-25","time":"22:00:12","open":177.38,"high":179.15,"low":175.82,"close":178.61,"volume":51301703,"name":"APPLE"}]}
-            result_dict = json_obj.get("symbols",[None])[0]            
-            return StooqObject(result_dict)
-        except (requests.exceptions.HTTPError, StopIteration):
-            return None
+        params = {"s": code, "f": "sd2t2ohlcvn", "e": "csv", "h": ""}
+        with requests.get(
+            "{}{}".format(self.base_url, self.resource_path), params=params, stream=True
+        ) as response:
+            try:
+                response.raise_for_status()
+
+                lines = (line.decode("utf-8") for line in response.iter_lines())
+                reader = csv.DictReader(lines)
+                result_dict = next(reader)
+                return StooqObject(result_dict)
+            except (requests.exceptions.HTTPError, StopIteration):
+                return None
